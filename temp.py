a20464b80b916f50d34da2ebfd47e76ba5dcb455
David <32776482+dawei-liang@users.noreply.github.com>
2019-12-01 12:38:35 -0600

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 30 21:39:56 2019
+Created on Sun Dec  1 12:30:50 2019
 
-@author: dawei
+@author: david
 """
 
 import pandas as pd
@@ -14,8 +13,9 @@ from keras.models import Sequential
 from keras.optimizers import SGD, Adadelta
 from keras.losses import mean_squared_error
 
-def cnn_model_fn():
+def cnn_model_fn(feature_shape):
         model = Sequential()
+
         model.add(Flatten())
         model.add(Dense(64, activation ='relu')) # Do not add batchnorm in dense, break linear
         #model.add(Dropout(0.2))
@@ -33,24 +33,45 @@ def cnn_model_fn():
                   optimizer=opt,
                   metrics=['mae'])
         return model
-    
+
+##################
+# Given a new dataframe data
+        
 data.dropna(how='any')
-state_action_pairs = data.iloc[:,0:-1].values
-rewards = data.iloc[:,-1].values
+state_action_pairs = data[['gender', 
+                            'amount', 
+                            'num_loan', 
+                            'duration', 
+                            'year_ratio', 
+                            'diff_city', 
+                            'marriage', 
+                            'kids', 
+                            'month_in', 
+                            'housing', 
+                            'edu',
+                            'motivation',
+                            'state_done',
+                            'cumulative_overdue_early_difference',
+                            'action']].values
+rewards = data['reward'].values
+
 # feature standization
 for j in range(state_action_pairs.shape[1]):
-    mean = 88
-    std = 209
+    mean = np.mean(state_action_pairs[:,j])
+    std = np.std(state_action_pairs[:,j])
+    print('mean, std of data column %d: %f %f' % (j, mean, std))
     for i in range(state_action_pairs.shape[0]):
         state_action_pairs[i,j] = (state_action_pairs[i,j] - mean) / std
+        
+# reward standizatioin
+print('mean, std of rewards: %f %f' % (np.mean(rewards), np.std(rewards)))   # 88, 209
+# reward standization
+rewards = (rewards - np.mean(rewards)) / np.std(rewards)
 
 model = cnn_model_fn()        
 state_action_pairs = np.reshape(state_action_pairs, (state_action_pairs.shape[0], state_action_pairs.shape[1], 1))
-model.load_weights('./' + '0-reg-0.2548.hdf5')
-pred = model.predict(state_action_pairs)
-# project back to original reward scale
-pred_mod = pred * np.std(rewards) + np.mean(rewards)
-for i in range(len(pred_mod)):
-    if pred_mod[i] < 100:
-        pred_mod[i] = 0
-    
+model.load_weights('./' + '0-reg-0.0110.hdf5')
+# no batch if predicting a single data
+pred = model.predict(state_action_pairs, batch_size=64)
+# final prediction, projected back to original reward scale
+pred_final = pred * 208.725478 + 88.320939
